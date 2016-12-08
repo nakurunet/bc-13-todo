a9e4d961973f10181b56360ce0c5fd13c3c1917f
Philip On <nakurunet@gmail.com>
2016-12-08 08:36:49 +0300

    An update of the login page

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request
 from flask import render_template
-from flask import redirect
+from flask import redirect, session
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import g, url_for
+import os
+"""
+To do list for andela bootcamp using flask  sessions 
+"""
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lists.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,15 +25,25 @@ class Task(db.Model):
     def __repr__(self):
         return '<Content %s>' % self.content
 
-
 db.create_all()
 
 
-@app.route('/')
-def tasks_list():
-    tasks = Task.query.all()
-    return render_template('list.html', tasks=tasks)
+@app.route('/', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		session.pop('user',None)
+		if request.form['password'] == 'MasterPass' and request.form['username'] == 'Admin' :
+			session['user'] = request.form['username']
+			return redirect(url_for('list'))
+	return render_template('login.html')
 
+
+@app.route('/list')
+def list():
+        if g.user:
+                tasks = Task.query.all()
+                return render_template('list.html', tasks=tasks)
+        
 
 @app.route('/task', methods=['POST'])
 def add_task():
@@ -40,8 +54,7 @@ def add_task():
     task = Task(content)
     db.session.add(task)
     db.session.commit()
-    return redirect('/')
-
+    return redirect('/list')
 
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
@@ -51,8 +64,7 @@ def delete_task(task_id):
 
     db.session.delete(task)
     db.session.commit()
-    return redirect('/')
-
+    return redirect('/list')
 
 @app.route('/done/<int:task_id>')
 def resolve_task(task_id):
@@ -66,8 +78,26 @@ def resolve_task(task_id):
         task.done = True
 
     db.session.commit()
-    return redirect('/')
-
-
+    return redirect('/list')
+	
+@app.before_request
+def before_request():
+	g.user = None
+	if 'user' in session:
+		g.user = session['user']
+		
+@app.route('/getsession')
+def getsession():
+	if 'user' in session:
+		return session['user']
+	return 'in'
+		
+@app.route('/dropsession')
+def dropsession():
+	session.pop('user', None)
+	return redirect('/')
+	
 if __name__ == '__main__':
-    app.run()
+	app.run (debug=True)
+		
+		
