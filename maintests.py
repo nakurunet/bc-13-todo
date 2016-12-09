d5749f842841535b87fa24546f21f50aa489de3e
Philip On <nakurunet@gmail.com>
2016-12-09 07:47:11 +0300

    Added tests to testcase

--- maintests.py
+++ maintests.py
@@ -11,35 +11,36 @@ class todoTests(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
         with app.list():
+            app.login()
 		
 # sends HTTP GET request to the application
 # to get 200 response
-	def test_page_status(self):
+    def test_page_status(self):
         result = self.app.get('/') 
         self.assertEqual(result.status_code, 200) 
 #test the login function	
-	def test_login_set(self, username, password):
-    return self.app.post('/login', data=dict(
-        username=username,
-        password=password
-    ), follow_redirects=True)
-	def test_login(self):
-    logtst = self.login('Admin', 'MasterPass')
-    assert 'You were logged in' in logtst.data
-    logtst = self.logout()
+    def test_login_set(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+    def test_login(self):
+        logtst = self.login('Admin', 'MasterPass')
+        assert 'You were logged in' in logtst.data
+        logtst = self.logout()
 #test the add task function		
-	def test_add_task(self):
-	rv = add_task()
-	assertRedirects(rv, url_for('list.html'))
+    def test_add_task(self):
+        rv = add_task()
+        assertRedirects(rv, url_for('list.html'))
 #test the delete tasks function		
-	def test_delete_task(self):
-		self.assertTrue(delete_task().task,True)
-		dellist = delete_task(self)
+    def test_delete_task(self):
+        self.assertTrue(delete_task().task,True)
+        dellist = delete_task(self)
         assertRedirects(dellist, url_for('list.html'))
 #test the task resolve function		
-	def test_resolve_task(self):
-		resv = resolve_task()
-	assertRedirects(resv, url_for('list.html'))
+    def test_resolve_task(self):
+        resv = resolve_task()
+        assertRedirects(resv, url_for('list.html'))
 		
 if __name__ == '__main__':
     unittest.main()
